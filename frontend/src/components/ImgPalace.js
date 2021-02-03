4cb2feba24f65946268049ffb147a0cc7349dca0
João Valente <joaohenrique90@sapo.pt>
2021-02-03 13:34:00 +0000

    Removal of unnecessary files.

--- frontend/src/components/ImgPalace.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import palace from '../palace.png';
-import './ImgPalace.css';
-
-const imgPalace = () => {
-  return <img id="imgPalace" src={palace}></img>;
-};
-
-export default imgPalace;
